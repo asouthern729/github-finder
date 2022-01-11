e8b07c3c78296b03ae02faffd1ceb432d67d9c91
asouthern729 <andrew.southern@franklintn.gov>
2022-01-11 16:32:30 -0600

    Setup GitHub context

--- src/App.js
+++ src/App.js
@@ -3,22 +3,25 @@ import Home from './pages/Home'
 import About from './pages/About'
 import NotFound from './pages/NotFound'
 import Navbar from './components/layout/Navbar'
+import { GitHubProvider } from './context/github/GitHubContext'
 
 function App() {
   return (
-    <Router>
-      <div className="flex flex-col justify-between h-screen">
-        <Navbar />
-        <main className="container mx-auto px-3 h-screen pb-12">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/notfound" element={<NotFound />} />
-            <Route path="/*" element={<NotFound />} />
-          </Routes>
-        </main>
-      </div>
-    </Router>
+    <GitHubProvider>
+      <Router>
+        <div className="flex flex-col justify-between h-screen">
+          <Navbar />
+          <main className="container mx-auto px-3 h-screen pb-12">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/notfound" element={<NotFound />} />
+              <Route path="/*" element={<NotFound />} />
+            </Routes>
+          </main>
+        </div>
+      </Router>
+    </GitHubProvider>
   );
 }
 
