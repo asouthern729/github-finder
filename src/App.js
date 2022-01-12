93d28681815916ab4cd8ee10b42f225f150defb5
asouthern729 <andrew.southern@franklintn.gov>
2022-01-12 14:30:53 -0600

    Alert context and reducer

--- src/App.js
+++ src/App.js
@@ -4,23 +4,26 @@ import About from './pages/About'
 import NotFound from './pages/NotFound'
 import Navbar from './components/layout/Navbar'
 import { GitHubProvider } from './context/github/GitHubContext'
+import { AlertProvider } from './context/alert/AlertContext'
 
 function App() {
   return (
     <GitHubProvider>
-      <Router>
-        <div className="flex flex-col justify-between h-screen">
-          <Navbar />
-          <main className="container mx-auto px-3 h-screen pb-12">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/notfound" element={<NotFound />} />
-              <Route path="/*" element={<NotFound />} />
-            </Routes>
-          </main>
-        </div>
-      </Router>
+      <AlertProvider>
+        <Router>
+          <div className="flex flex-col justify-between h-screen">
+            <Navbar />
+            <main className="container mx-auto px-3 h-screen pb-12">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/notfound" element={<NotFound />} />
+                <Route path="/*" element={<NotFound />} />
+              </Routes>
+            </main>
+          </div>
+        </Router>
+      </AlertProvider>
     </GitHubProvider>
   );
 }
